d0f13ec5eee41761bc04935668a9ec585f584c37
Alhaji Aki <abdulkudus2922@gmail.com>
2023-07-02 05:31:34 +0000

    force json response

---
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ForceJsonResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('accept', 'application/json');
+
+        return $next($request);
+    }
+}
