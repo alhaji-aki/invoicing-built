a1560d6b6c39e7a0b55e62a2e54aab353f482a68
alhaji-aki <abdulkudus2922@gmail.com>
2024-04-29 07:50:35 +0000

    WIP

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -12,6 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('web')
+                ->prefix('webhooks')
+                ->name('webhooks.')
+                ->group(base_path('routes/webhook.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(
